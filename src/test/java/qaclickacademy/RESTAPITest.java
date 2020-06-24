8bbae8574e805dbb35fb1dbf5a7b5ecf0b6541b4
Gurpreet Sondhi <gurpreetsondhi1@gmail.com>
2020-06-24 22:25:24 +0530

    Third Commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -8,7 +8,10 @@ public class RESTAPITest {
 	@Test
 	public void postJira() 
 	{
-		System.out.println("postJira");
+		System.out.println("postJira : 1");
+		System.out.println("postJira : 2");
+		System.out.println("postJira : 3");
+		System.out.println("postJira : 4");
 	}
 	
 
